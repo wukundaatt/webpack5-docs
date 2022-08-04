07252ec807278c303b5b97a60f50de1e77f86dbb
wukunda <991360817@qq.com>
2022-08-04 22:31:27 +0800

    'init'

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # webpack5-docs
+
+快速开始：
+
+运行以下命令行：
+
+1. npm i
+2. npm run dev (yarn dev)
+
---
+++ docs/.vuepress/.temp/internal/clientAppEnhances.js
@@ -0,0 +1,15 @@
+import clientAppEnhance0 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/node_modules/@vuepress/plugin-medium-zoom/lib/client/clientAppEnhance.js'
+import clientAppEnhance1 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/node_modules/@vuepress/plugin-theme-data/lib/client/clientAppEnhance.js'
+import clientAppEnhance2 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/node_modules/@vuepress/theme-default/lib/client/clientAppEnhance.js'
+import clientAppEnhance3 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/docs/.vuepress/.temp/sass-palette/load-hope.js'
+import clientAppEnhance4 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/node_modules/@vuepress/plugin-external-link-icon/lib/client/clientAppEnhance.js'
+import clientAppEnhance5 from 'C:/Users/Administrator/Desktop/workspace/VBlog/webpack/node_modules/@vuepress/plugin-search/lib/client/clientAppEnhance.js'
+
+export const clientAppEnhances = [
+  clientAppEnhance0,
+  clientAppEnhance1,
+  clientAppEnhance2,
+  clientAppEnhance3,
+  clientAppEnhance4,
+  clientAppEnhance5,
+]
